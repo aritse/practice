3a3852dcb2ad917107e2edc08f8b868343ce160e
Ari <tserenda@hotmail.com>
2018-11-28 08:12:46 +0800

    merge sort

--- sort.c
+++ sort.c
@@ -55,20 +55,66 @@ void insertion_sort(int A[], int n)
         }
 }
 
+void merge(int A[], int start, int mid, int end)
+{
+    int p = start, q = mid + 1;
+    int T[end - start + 1], k = 0;
+
+    for (int i = start; i <= end; i++)
+    {
+        if (p > mid)
+            T[k++] = A[q++];
+        else if (q > end)
+            T[k++] = A[p++];
+        else if (A[p] < A[q])
+            T[k++] = A[p++];
+        else
+            T[k++] = A[q++];
+    }
+
+    for (int i = 0; i < k; i++)
+        A[start++] = T[i];
+}
+
+void merge_sort(int A[], int start, int end)
+{
+    /* Merge sort is a divide-and-conquer algorithm based on the idea of
+    breaking down a list into several sub-lists until each sublist consists
+    of a single element and merging those sublists in a manner that
+    results into a sorted list. */
+    if (start < end)
+    {
+        int mid = (start + end) / 2;
+        merge_sort(A, start, mid);
+        merge_sort(A, mid + 1, end);
+        merge(A, start, mid, end);
+    }
+}
+
 int main()
 {
     int n = 20, A[n];
+
+    printf("Unsorted:\n");
     for (int i = 0; i < n; i++)
     {
         A[i] = rand() % 10;
         printf("%d ", A[i]);
     }
+    printf("\n");
+
     // printf("\nBubble Sort:\n");
     // bubble_sort(A, n);
+
     // printf("\nSelection Sort:\n");
     // selection_sort(A, n);
-    printf("\nInsertion Sort:\n");
-    insertion_sort(A, n);
+
+    // printf("\nInsertion Sort:\n");
+    // insertion_sort(A, n);
+
+    printf("\nMerge Sort:\n");
+    merge_sort(A, 0, n - 1);
+
     for (int i = 0; i < n; i++)
         printf("%d ", A[i]);
     printf("\n");
