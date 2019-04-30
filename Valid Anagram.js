d30d8d4e8241b6a113a69507fcb36291aeb857b6
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-04-29 19:40:52 -0700

    Valid Anagram

---
+++ Valid Anagram.js
@@ -0,0 +1,23 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  s = Array.from(s).sort();
+  t = Array.from(t).sort();
+  for (i in s) {
+    if (s[i] != t[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+var s = "rat";
+var t = "car";
+console.log(isAnagram(s, t));
