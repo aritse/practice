b4677def30933aaf080281a86430b3bd8aa58fba
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-04-29 19:46:54 -0700

    Fixed a bug

--- Valid Anagram.js
+++ Valid Anagram.js
@@ -8,6 +8,9 @@ Given two strings s and t , write a function to determine if t is an anagram of 
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+  if (s.length != t.length) {
+    return false;
+  }
   s = Array.from(s).sort();
   t = Array.from(t).sort();
   for (i in s) {
