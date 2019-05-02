be280314bdd1a1a5528eeb7a3db1bd76fbdeb4bc
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-05-01 17:42:48 -0700

    TypeScript in 5 min

---
+++ TypeScript/greeter.ts
@@ -0,0 +1,23 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+let user = new Student("Bob", "S.", "Hardy");
+
+document.body.innerHTML = greeter(user);
