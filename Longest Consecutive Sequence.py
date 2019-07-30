e174b9c0b468e6d0f5a3c21069cbbc9508fc97fc
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-07-30 15:16:01 -0700

    Longest Consecutive Sequence

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -1,22 +1,17 @@
-# Given an unsorted array of integers, find the length of the longest consecutive elements sequence. Your algorithm should run in O(n) complexity.
+# Given an unsorted array of integers, find the length of the longest
+# consecutive elements sequence. Your algorithm should run in O(n) complexity.
 
 
 class Solution(object):
     def longestConsecutive(self, nums):
-        if len(nums) == 0:
-            return 0
-        maximum = max(nums)
-        flags = [False] * (maximum+1)
-        for n in nums:
-            flags[n] = True
-        longest = 1
-        current = 0
-        for flag in flags:
-            if flag:
-                current += 1
-            else:
-                if current > longest:
-                    longest = current
-                current = 0
-
+        s = set(nums)
+        longest = 0
+        for num in nums:
+            if num + 1 in s:
+                continue
+            count = 0
+            while num in s:
+                count += 1
+                num -= 1
+            longest = max(longest, count)
         return longest
