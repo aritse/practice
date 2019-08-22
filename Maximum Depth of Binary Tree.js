2a034434982ef363dbd2b8e1d8ce20f65a558706
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-08-22 13:24:05 -0700

    Maximum Depth of Binary Tree

---
+++ Maximum Depth of Binary Tree.js
@@ -0,0 +1,24 @@
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Note: A leaf is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root) {
+    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+  }
+  return 0;
+};
---
+++ Maximum Depth of Binary Tree.py
@@ -0,0 +1,23 @@
+# Given a binary tree, find its maximum depth.
+
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Note: A leaf is a node with no children.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root:
+            return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)
+        return 0
