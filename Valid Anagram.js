abad82f0f6e2bd5f3a9552b51d4dabf3bd4eb453
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-08-23 15:00:26 -0700

    Valid Anagram.js

--- Valid Anagram.js
+++ Valid Anagram.js
@@ -1,26 +1,35 @@
-/*
-Given two strings s and t , write a function to determine if t is an anagram of s.
-*/
+// Given two strings s and t , write a function to determine if t is an anagram of s.
 
+/* My solution 1 */
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-  if (s.length != t.length) {
-    return false;
-  }
-  s = Array.from(s).sort();
-  t = Array.from(t).sort();
-  for (i in s) {
-    if (s[i] != t[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// var isAnagram = function(s, t) {
+//   if (s.length != t.length) {
+//     return false;
+//   }
+//   s = Array.from(s).sort();
+//   t = Array.from(t).sort();
+//   for (i in s) {
+//     if (s[i] != t[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-var s = "rat";
-var t = "car";
-console.log(isAnagram(s, t));
+/* My solution 2 */
+var isAnagram = function(s, t) {
+  return (
+    s
+      .split("")
+      .sort()
+      .join() ===
+    t
+      .split("")
+      .sort()
+      .join()
+  );
+};
