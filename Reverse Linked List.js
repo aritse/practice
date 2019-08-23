499a29f3d733b94a7777ef85f6cb965045911428
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-08-22 19:11:57 -0700

    Reverse Linked List

---
+++ Reverse Linked List.js
@@ -0,0 +1,51 @@
+// Reverse a singly linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/* My solution */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// var reverseList = function(head) {
+//   if (!head) {
+//     return null;
+//   }
+
+//   // save address of each node
+//   pointers = [];
+//   while (head.next) {
+//     pointers.push(head);
+//     head = head.next;
+//   }
+//   pointers.push(head);
+
+//   // redirect the next pointer
+//   len = pointers.length;
+//   for (i = len - 1; i > 0; i--) {
+//     pointers[i].next = pointers[i - 1];
+//   }
+//   pointers[0].next = null;
+
+//   return head;
+// };
+
+/* Solution that I found it cool */
+
+var reverseList = function(head) {
+  prev = null;
+  while (head) {
+    temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+  return prev;
+};
