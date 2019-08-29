82d74dcbca90cdc719288accdb2107e30d853179
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-08-29 10:50:31 -0700

    Convert Sorted Array to Binary Search Tree.js

--- Convert Sorted Array to Binary Search Tree.js
+++ Convert Sorted Array to Binary Search Tree.js
@@ -9,19 +9,16 @@
  *     this.left = this.right = null;
  * }
  */
+
 /**
  * @param {number[]} nums
  * @return {TreeNode}
  */
 var sortedArrayToBST = function(nums) {
-  const len = nums.length;
-  if (!len) {
-    return null;
-  }
-
-  const mid = Math.floor(len / 2);
-  const node = new TreeNode(nums[mid]);
+  if (!nums.length) return null;
+  var mid = Math.floor(nums.length / 2);
+  var node = new TreeNode(nums[mid]);
   node.left = sortedArrayToBST(nums.slice(0, mid));
-  node.right = sortedArrayToBST(nums.slice(mid + 1, len));
+  node.right = sortedArrayToBST(nums.slice(mid + 1));
   return node;
 };
