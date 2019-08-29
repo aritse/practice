3e393b8eb4d4a51a6592d426d72f61bd6f2a63c5
Ari Tserendavaa (Digital Intelligence Systems) <v-artser@microsoft.com>
2019-08-29 15:35:27 -0700

    First Unique Character in a String.js

---
+++ First Unique Character in a String.js
@@ -0,0 +1,31 @@
+// Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/* My solution */
+
+var firstUniqChar = function(s) {
+  var dict = {};
+  for (c of s) {
+    if (dict[c]) dict[c]++;
+    else dict[c] = 1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (dict[s[i]] === 1) return i;
+  }
+  return -1;
+};
+
+/* Solution that I found it online cool */
+
+// var firstUniqChar = function(s) {
+//   for (i = 0; i < s.length; i++) {
+//     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
