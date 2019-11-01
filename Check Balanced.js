928e9d3afd5126f71d459d1803be6414d0d5519d
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-01 11:49:59 -0700

    Add optimization and some formatting

--- Check Balanced.js
+++ Check Balanced.js
@@ -3,16 +3,24 @@ Implement a function to check if a binary tree is balanced. For the purposes of 
 tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
 */
 
-var getHeight = root => root ? 1 + Math.max(getHeight(root.left), getHeight(root.right)) : 0;
+var height = root => root ? 1 + Math.max(height(root.left), height(root.right)) : 0;
 
-var isBalanced = root => {
-    if (root) {
-        const diff = Math.abs(getHeight(root.left) - getHeight(root.right));
-        return diff < 2 && isBalanced(root.left) && isBalanced(root.right);
-    }
-    return true;
+// Unoptimized version
+var isBalanced2 = root => root ? Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced2(root.left) && isBalanced2(root.right) : true;
+
+var checkHeight = root => {
+    if (!root) return 0;
+    const leftHeight = checkHeight(root.left);
+    if (leftHeight < 0) return -1;
+    const rightHeight = checkHeight(root.right);
+    if (rightHeight < 0) return -1;
+    return Math.abs(leftHeight - rightHeight) < 2 ? Math.max(leftHeight, rightHeight) + 1 : -1;
 };
 
+// Optimized version
+var isBalanced = root => checkHeight(root) < 0 ? false : true;
+
+/* Utility functions */
 var createMinBST = function (numbers, start, end) {
     if (end < start) return null;
     const mid = Math.floor((start + end) / 2);
@@ -47,6 +55,8 @@ var inorder = function (root) {
         inorder(root.right);
     }
 };
+
+/* Driver */
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 tree = createMinBST(numbers, 0, numbers.length - 1);
 inorder(tree);
