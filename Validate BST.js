39da6fa2dc2c5415914aee7ecd2107046758ec33
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-01 13:13:31 -0700

    It works on trees with duplicate values

--- Validate BST.js
+++ Validate BST.js
@@ -4,7 +4,13 @@ import { createMinBST } from './Tree';
 Implement a function to check if a binary tree is a binary search tree.
 */
 
-var isBST = root => {
+var isBST = function (root, min, max) {
+    if (!root) return true;
+    if ((min && root.value <= min) || (max && root.value > max)) return false;
+    return isBST(root.left, min, root.value) && isBST(root.right, root.value, max);
+};
+
+var isBST2 = root => {
     var visitNode = value => traversal.push(value);
 
     var inorder = function (root) {
@@ -26,6 +32,8 @@ var isBST = root => {
 /* Driver */
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const tree = createMinBST(numbers);
-console.log(isBST(tree));
+// console.log(isBST2(tree));
+console.log(isBST(tree, null, null));
 tree.value = 10;
-console.log(isBST(tree));
+// console.log(isBST2(tree));
+console.log(isBST(tree, null, null));
