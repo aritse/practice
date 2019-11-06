470e5780c1cdda2494477afeced857ed3bf4914a
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-06 13:46:18 -0800

    Made it simpler

--- Delete Middle Node.js
+++ Delete Middle Node.js
@@ -4,14 +4,13 @@ Implememnt an algorith to delete a node in the middle of a singly linked list
 given only access to that node */
 
 var deleteMiddleNode = function (node) {
-    let temp = node.next;
-    while (temp.next !== null) {
-        node.data = temp.data;
-        node = temp;
-        temp = temp.next;
+    if (node === null || node.next === null) {
+        return false;
     }
-    node.data = temp.data;
-    node.next = null;
+    const next = node.next;
+    node.data = next.data;
+    node.next = next.next;
+    return true;
 };
 
 const linkedList = new LinkedList();
@@ -22,14 +21,17 @@ console.log("Original linked list:");
 console.log(linkedList.toString());
 console.log("Length: ", linkedList.length);
 
-const k = 17;
+const k = 2;
 const kth = linkedList.findKthFromLast(k);
 if (kth !== null) {
-    console.log("\nRemoving k = ", k, " from last: ", kth.data);
-    deleteMiddleNode(kth);
-    linkedList.length--;
-    console.log(linkedList.toString());
-    console.log("Length: ", linkedList.length);
+    console.log("\nRemoving k = ", k, "from last: ", kth.data);
+    if (deleteMiddleNode(kth)) {
+        linkedList.length--;
+        console.log(linkedList.toString());
+        console.log("Length: ", linkedList.length);
+    } else {
+        console.log("Error: please provide the middle node.");
+    }
 } else {
     console.log("element from last k = ", k, ": Out of bound");
 }
