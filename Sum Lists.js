18c1cd242bbded01203e9b6d16f6545a043ec2ba
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-13 14:18:57 -0800

    Sum Lists.js

--- Sum Lists.js
+++ Sum Lists.js
@@ -11,40 +11,98 @@ Input: (6->1->7) + (2->9->5). That is 617+295
 Output: 9->1->2. That is 912.
 */
 
-import { LinkedList } from "./Linked List"
+import { LinkedList } from "./Linked List"; // Run as: node -r esm "Sum Lists.js"
 
 /**
  * 
- * @param {LinkedList} a 
- * @param {LinkedList} b 
+ * @param {LinkedList} listA
+ * @param {LinkedList} listB
  * @returns {LinkedList}
  */
-var addNumbers = function (listA, listB) {
+var addNumbersInReverse = function (listA, listB) {
+    let [nodeA, nodeB] = [listA.head, listB.head];
     const sum = new LinkedList();
     let a, b, carry = 0;
-    while (listA || listB) {
-        a = listA ? listA.data : 0;
-        b = listB ? listB.data : 0;
+    while (nodeA || nodeB) {
+        a = nodeA ? nodeA.data : 0;
+        b = nodeB ? nodeB.data : 0;
         let s = a + b + carry;
         carry = s > 9 ? 1 : 0;
         s %= 10;
         sum.append(s);
-        listA = listA ? listA.next : null;
-        listB = listB ? listB.next : null;
+        nodeA = nodeA ? nodeA.next : null;
+        nodeB = nodeB ? nodeB.next : null;
     }
     return sum;
 }
 
+var addNumbersInForward = function (listA, listB) {
+    var reverseBoth = function (nodeA, nodeB) {
+        if (nodeA && nodeB) {
+            reverseBoth(nodeA.next, nodeB.next);
+            let s = nodeA.data + nodeB.data + carry;
+            carry = s > 9 ? 1 : 0;
+            s %= 10;
+            sum.add(s);
+        }
+    }
+
+    var reverseLonger = function (node, diff) {
+        if (diff < 1) return;
+        diff--;
+        reverseLonger(node, diff);
+        if (carry) {
+            sum.add(node.data + carry);
+            carry = 0;
+        }
+        sum.add(node.data);
+    }
+
+    // advance pointer of the longer list by diff amount
+    let [runnerA, runnerB] = [listA.head, listB.head];
+    let [lenA, lenB] = [listA.length(), listB.length()];
+    let diff = Math.abs(lenA - lenB);
+    while (lenA > lenB && diff--) runnerA = runnerA.next;
+    while (lenA < lenB && diff--) runnerB = runnerB.next;
+
+    // add digits as reversing
+    const sum = new LinkedList();
+    let carry = 0;
+    reverseBoth(runnerA, runnerB);
+
+    // concat the rest of the longer list to sum
+    const longer = lenA < lenB ? listB : listA;
+    reverseLonger(longer.head, Math.abs(lenA - lenB));
+
+    return sum;
+}
+
 // Driver
+
+// Build linked lists
 const listA = new LinkedList();
-listA.add(6);
-listA.add(1);
-listA.add(7);
-console.log(listA.toString());
+listA.append(6);
+listA.append(1);
+listA.append(7);
+listA.append(3);
 const listB = new LinkedList();
-listB.add(2);
-listB.add(9);
-listB.add(5);
-console.log(listB.toString());
-let sum = addNumbers(listA.head, listB.head);
-console.log(sum.toString());
+listB.append(2);
+listB.append(9);
+listB.append(5);
+
+// Print linked lists
+console.log("list A: ", listA.toString());
+console.log("list B: ", listB.toString());
+
+// Add numbers in reverse
+// Input: (6->1->7->3) + (2->9->5). That is, 3716 + 592.
+// Output: 8->0->3->4. That is, 4308.
+const sumInReverse = addNumbersInReverse(listA, listB);
+console.log("Sum in Reverse: ", sumInReverse.toString());
+
+
+// Add numbers in forward
+// Input: (6->1->7->3) + (2->9->5). That is, 6173 + 295.
+// Output: 6->4->6->8. That is, 6468.
+const sumInForward = addNumbersInForward(listA, listB);
+console.log("Sum in Forward: ", sumInForward.toString());
