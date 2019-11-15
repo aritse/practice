2785c259e1a92d55d9f1460c1b77b6b0891c158e
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-14 23:08:05 -0800

    Implemented popAt(index) function

--- Stack of Plates.js
+++ Stack of Plates.js
@@ -1,6 +1,7 @@
 /* DESCRIPTION
 Implement a data structure of a set of stacks. When a sub stack size reaches a certain threshold, a new sub stack is
-created. */
+created. Implement push() and pop() functions. As a follow up, implement popAt(index) function to pop from
+a specific sub stack at index. */
 
 class SetOfStacks {
     constructor(threshold = 3) {
@@ -20,16 +21,30 @@ class SetOfStacks {
             this.stacks.pop();
         return this.stacks[this.stacks.length - 1].pop();
     }
+
+    popAt(index) {
+        if (index < 0 || index >= this.stacks.length) throw Error("index out of bound");
+        const p = this.stacks[index].pop();
+        if (this.stacks[index].length === 0)
+            this.stacks.splice(index, 1);
+        return p;
+    }
 }
 
+// Driver
 const setOfStacks = new SetOfStacks();
-
 for (let i = 0; i < 13; i++)
     setOfStacks.push(i);
-
 console.log(setOfStacks);
 setOfStacks.pop();
 setOfStacks.pop();
 setOfStacks.pop();
 setOfStacks.push(99);
+console.log(setOfStacks);
+setOfStacks.popAt(1);
+setOfStacks.popAt(1);
+setOfStacks.popAt(1);
+console.log("Popped: ", setOfStacks.popAt(1));
+setOfStacks.push(100);
+setOfStacks.push(200);
 console.log(setOfStacks);
