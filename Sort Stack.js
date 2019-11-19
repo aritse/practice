7623612b36768f3cb90718414e747773639bc3e5
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-19 11:03:03 -0800

    Explained the algorithm

--- Sort Stack.js
+++ Sort Stack.js
@@ -6,10 +6,10 @@ Write a program to sort a stack such that the smallest items are on the top. You
 const N = 20;
 
 var merge = function (a, b, c) {
-    while (!b.isEmpty() && !c.isEmpty())
+    while (b.isEmpty() === false && c.isEmpty() === false)
         if (b.peek() < c.peek())
             a.push(b.pop());
-    while (!a.isEmpty() || !c.isEmpty()) {
+    while (a.isEmpty() === false || c.isEmpty() === false) {
         if (a.isEmpty())
             b.push(c.pop());
         else if (c.isEmpty())
@@ -23,29 +23,36 @@ var merge = function (a, b, c) {
 };
 
 var helper = function (a, b, c) {
-    while (!a.isEmpty()) {
+    while (a.isEmpty() === false) {
         if (a.peek() >= c.peek())
             c.push(a.pop());
         else
             b.push(a.pop());
     }
-    const t = sort(b);
-    return merge(a, t, c);
+    b = sort(b);
+    return merge(a, b, c);
 };
 
-var sort = function (stack) {
-    if (!stack.isEmpty()) {
+var sort = function (a) {
+    if (a.isEmpty() === false) {
         const [b, c] = [new Stack(), new Stack()];
-        c.push(stack.pop());
-        stack = helper(stack, b, c);
+        c.push(a.pop());
+        a = helper(a, b, c);
     }
-    return stack;
+    return a;
 };
 
 // Driver
-let stack = new Stack();
+const unsorted = new Stack();
 for (let i = 0; i < N; i++)
-    stack.push(Math.random() * 10 | 0);
-console.log("Unsorted: ", stack.list());
-stack = sort(stack);
-console.log("Sorted: ", stack.list());
+    unsorted.push(Math.random() * 100 | 0);
+console.log("Unsorted: ", unsorted.list());
+const sorted = sort(unsorted);
+console.log("Sorted: ", sorted.list());
+
+/* EXPLANATION
+Setting: Think of 3 poles (A, B, C) with 10 unsorted rings stacked on A. B and C are empty.
+Step 1: Keep popping A onto B and C such that C is reverse sorted and B is unsorted.
+Step 2: Sort B recursively. When it returns, B is sorted.
+Step 3: Keep popping B onto A until the top of B is greater than the top of C.
+Step 4: Keep popping the larger of A and C onto B. */
