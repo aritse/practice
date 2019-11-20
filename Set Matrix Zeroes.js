e833eba3a4006aeb8e5fc50b700d53dc35756ccb
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2019-11-20 09:58:36 -0800

    Added a similar solution found online. No need to return the matrix.

--- Set Matrix Zeroes.js
+++ Set Matrix Zeroes.js
@@ -9,57 +9,65 @@ O(m + n) space, but still not the best solution. Could you devise a constant spa
  */
 var setZeroes = function (matrix) {
     const [m, n] = [matrix.length, matrix[0].length];
-
     for (let row = 0; row < m; row++) {
         for (let col = 0; col < n; col++) {
             if (matrix[row][col] === 0) {
-                for (let i = 0; i < m; i++)
-                    if (matrix[i][col] !== 0)
-                        matrix[i][col] = 0.5;
-                for (let i = 0; i < n; i++)
-                    if (matrix[row][i] !== 0)
-                        matrix[row][i] = 0.5;
+                for (let i = 0; i < m; i++)     matrix[i][col] = matrix[i][col] && 0.5;
+                for (let i = 0; i < n; i++)     matrix[row][i] = matrix[row][i] && 0.5;
             }
         }
     }
-
     for (let row = 0; row < m; row++)
         for (let col = 0; col < n; col++)
             matrix[row][col] = Math.floor(matrix[row][col]);
+};
 
-    return matrix;
+// Most voted JS solution on LC - but the run-time is still O(nm x (n+m)).
+var setZeroes2 = function (matrix) {
+    const [m, n] = [matrix.length, matrix[0].length];
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            if (Object.is(matrix[row][col], 0)) {
+                for (let i = 0; i < m; i++)     matrix[i][col] = matrix[i][col] && -0;
+                for (let i = 0; i < n; i++)     matrix[row][i] = matrix[row][i] && -0;
+            }
+        }
+    }
 };
 
 // Driver
-const matrix = [
+const A = [
     [1, 1, 1],
     [1, 0, 1],
     [1, 1, 1]
 ];
-console.log(setZeroes(matrix)); // Expected: [ [ 1, 0, 1 ], [ 0, 0, 0 ], [ 1, 0, 1 ] ]
+setZeroes(A);
+console.log(A); // Expected: [ [ 1, 0, 1 ], [ 0, 0, 0 ], [ 1, 0, 1 ] ]
 
-const matrix2 = [
+const B = [
     [0, 1, 2, 0],
     [3, 4, 5, 2],
     [1, 3, 1, 5]
 ];
-console.log(setZeroes(matrix2)); // Expected: [ [ 0, 0, 0, 0 ], [ 0, 4, 5, 0 ], [ 0, 3, 1, 0 ] ]
+setZeroes(B);
+console.log(B); // Expected: [ [ 0, 0, 0, 0 ], [ 0, 4, 5, 0 ], [ 0, 3, 1, 0 ] ]
 
-const matrix3 = [
+const C = [
     [0, 0, 0, 5],
     [4, 3, 1, 4],
     [0, 1, 1, 4],
     [1, 2, 1, 3],
     [0, 0, 1, 1]
 ];
-console.log(setZeroes(matrix3));
+setZeroes(C);
+console.log(C);
 /* Expected: [
-    [*, *, ., *],
-    [*, *, 1, 4],
-    [., *, 1, 4],
-    [*, *, 1, 3],
-    [., ., 1, 1]
+    [0, 0, 0, 0],
+    [0, 0, 0, 4],
+    [0, 0, 0, 0],
+    [0, 0, 0, 3],
+    [0, 0, 0, 0]
 ] */
 
 /* QUESTIONS
-Are the elements integers, double, characters and symbols? */
+Are the elements integers, double, characters and symbols? It looks like the test cases are in integers only. */
