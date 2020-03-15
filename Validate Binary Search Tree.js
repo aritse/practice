23413a11a276ab3891fa69c7afd5c58548b6c8fd
Ari Tserendavaa <tserenda@hotmail.com>
2020-03-15 10:02:28 -0700

    Validate Binary Search Tree.js

---
+++ Validate Binary Search Tree.js
@@ -0,0 +1,58 @@
+/* Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4. */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = root => {
+  if (root) {
+    const [left, right] = [inorder(root.left), inorder(root.right)];
+
+    const max = left.length > 0 ? left[left.length - 1] : -Infinity;
+    const min = right.length > 0 ? right[0] : Infinity;
+
+    if (isValidBST(root.left) && isValidBST(root.right)) {
+      return max < root.val && root.val < min ? true : false;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const inorder = node => (node ? [...inorder(node.left), node.val, ...inorder(node.right)] : []);
