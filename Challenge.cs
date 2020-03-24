4b292b6f6628d959547c6acc338438ab6adbeb88
Ari Tserendavaa <tserenda@hotmail.com>
2020-03-23 17:52:19 -0700

    accepts args on the command line

--- Challenge.cs
+++ Challenge.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Windows.Forms.HtmlElement;
 
 namespace DownloadPageWebClient
 {
@@ -11,26 +12,41 @@ namespace DownloadPageWebClient
         {
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             string url = args[0];
-            Int16 limit = 10;
+            Int16 limitBy = Int16.Parse(args[1]);
+            string[] excludeWords = new string[args.Length - 2];
+            for (int i = 2; i < args.Length; i++)
+            {
+                excludeWords[i - 2] = args[i];
+            }
+
             using (WebClient client = new WebClient())
             {
                 string html = client.DownloadString(url);
+                // HtmlElement el = GetElementById("History");
+                // Console.WriteLine(el);
+
                 string[] words = html.Split(' ');
                 Dictionary<string, Int16> count = new Dictionary<string, Int16>();
                 foreach (var word in words)
                 {
-                    if (count.ContainsKey(word))
+
+                    if (!excludeWords.Contains(word))
                     {
-                        count[word]++;
-                    }
-                    else
-                    {
-                        count.Add(word, 1);
+
+                        if (count.ContainsKey(word))
+                        {
+                            count[word]++;
+                        }
+                        else
+                        {
+                            count.Add(word, 1);
+                        }
                     }
                 }
 
-                var result = count.OrderByDescending(c => c.Value).Take(limit).Select(c => new { Word = c.Key, Count = c.Value });
+                var result = count.OrderByDescending(c => c.Value).Take(limitBy).Select(c => new { Word = c.Key, Count = c.Value });
                 foreach (var item in result)
                 {
                     Console.WriteLine(item);
