cc6bb3d5bc939cf393f3f2c36785b0a6ab5df62c
Ari Tserendavaa <tserenda@hotmail.com>
2020-04-22 23:48:06 -0700

    Leftmost Column with at Least a One.js

---
+++ Leftmost Column with at Least a One.js
@@ -0,0 +1,50 @@
+/** (This problem is an interactive problem.)
+
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix, this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix interface:
+
+BinaryMatrix.get(row, col) returns the element of the matrix at index (row, col) (0-indexed).
+BinaryMatrix.dimensions() returns a list of 2 elements [rows, cols], which means the matrix is rows * cols.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples. You will not have access the binary matrix directly. */
+
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function BinaryMatrix() {
+ *     @param {integer} row, col
+ *     @return {integer}
+ *     this.get = function(row, col) {
+ *         ...
+ *     };
+ *
+ *     @return {[integer, integer]}
+ *     this.dimensions = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {BinaryMatrix} binaryMatrix
+ * @return {number}
+ */
+function leftMostColumnWithOne(matrix) {
+  const [H, W] = matrix.dimensions();
+  let answer = W;
+  for (let row = 0; row < H; row++) {
+    let [low, high] = [0, W - 1];
+    while (low <= high) {
+      const mid = ((low + high) / 2) | 0;
+      if (matrix.get(row, mid) === 1) {
+        answer = Math.min(answer, mid);
+        high = mid - 1;
+      } else low = mid + 1;
+    }
+  }
+  return answer === W ? -1 : answer;
+}
