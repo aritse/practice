117ebaf6521d64c9f2a67b800fae8cea611f8b31
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2020-05-18 19:20:33 -0700

    .\Permutation in String.js

---
+++ Permutation in String.js
@@ -0,0 +1,45 @@
+/**Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+ 
+
+Note:
+
+The input strings only contain lower case letters.
+The length of both given strings is in range [1, 10,000]. */
+
+const checkInclusion = function (s1, s2) {
+    const s1Freq = frequency(s1);
+    for (let i = 0; i <= s2.length - s1.length; i++) {
+        const s2Freq = frequency(s2.substr(i, s1.length));
+        if (isEqual(s1Freq, s2Freq)) return true;
+    }
+    return false;
+};
+
+const frequency = function (s) {
+    const freq = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 97]++;
+    }
+    return freq;
+};
+
+const isEqual = function (arr1, arr2) {
+    for (let i = 0; i < 26; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
