a402dece8cbacadabcf50b620ff5b7cbbde503a6
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2020-05-19 19:51:46 -0700

    .\Valid Perfect Square.js

---
+++ Valid Perfect Square.js
@@ -0,0 +1,31 @@
+/** Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+ 
+
+Example 1:
+
+Input: num = 16
+Output: true
+Example 2:
+
+Input: num = 14
+Output: false
+ 
+
+Constraints:
+
+1 <= num <= 2^31 - 1 */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const isPerfectSquare = function (num) {
+    if (num === 1) return true;
+    for (let i = 0; i <= (num + 1) / 2; i++) {
+        if (i * i === num) return true;
+    }
+    return false;
+};
