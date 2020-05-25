f6958e0d56a604724aeb02077bc70e27328d335b
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2020-05-24 21:48:48 -0700

    .\Interval List Intersections.js

---
+++ Interval List Intersections.js
@@ -0,0 +1,30 @@
+/** Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists. */
+
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+var intervalIntersection = function (A, B) {
+    let [a, b] = [0, 0];
+    const I = [];
+    while (a < A.length && b < B.length) {
+        if (
+            (A[a][0] < B[b][1] && A[a][1] >= B[b][0]) ||
+            (B[b][0] < A[a][1] && B[b][1] >= A[a][0])
+        ) {
+            I.push([Math.max(A[a][0], B[b][0]), Math.min(A[a][1], B[b][1])]);
+        }
+        if (A[a][1] > B[b][1]) b++;
+        else a++;
+    }
+    return I;
+};
