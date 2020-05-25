30fd3efe5306af3237ae9976f98bcc79e07299f4
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2020-05-25 11:33:37 -0700

    .\Loop Detection.js

--- Loop Detection.js
+++ Loop Detection.js
@@ -15,10 +15,13 @@ function findLoop(head) {
     let [slow, fast] = [head, head];
     while (fast && fast.next) {
         if (slow === fast) {
-            return slow;
+            return fast;
         }
         slow = slow.next;
         fast = fast.next.next;
     }
     return null;
 }
+
+// Really good video explaining the mathematical proof
+// https://www.youtube.com/watch?v=9YTjXqqJEFE&t=65s
